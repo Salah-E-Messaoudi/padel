feb7346b2488b9e530578eff49b8c4944c856a95
Oussama MESSAOUDI <oussama.messaoudi.info@gmail.com>
2022-04-15 12:41:27 +0100

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 // import 'package:firebase_core/firebase_core.dart';
 // import 'package:firebase_messaging/firebase_messaging.dart';
 // import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
-// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-//   await Firebase.initializeApp();
-// }
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
 
-// late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
-// late AndroidNotificationChannel channel;
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+late AndroidNotificationChannel channel;
 
 void main() async {
   final settingsController = SettingsController(SettingsService());
@@ -23,25 +26,25 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  //await Firebase.initializeApp();
-  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  // channel = const AndroidNotificationChannel(
-  //   'channel_id', // id
-  //   'channel_title', // title
-  //   description: 'channel_description', // description
-  //   importance: Importance.high,
-  //   playSound: true,
-  //   showBadge: true,
-  // );
-  // flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  // await flutterLocalNotificationsPlugin
-  //     .resolvePlatformSpecificImplementation<
-  //         AndroidFlutterLocalNotificationsPlugin>()
-  //     ?.createNotificationChannel(channel);
-  // await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-  //   alert: true,
-  //   badge: true,
-  //   sound: true,
-  // );
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  channel = const AndroidNotificationChannel(
+    'channel_id',
+    'channel_title',
+    description: 'channel_description',
+    importance: Importance.high,
+    playSound: true,
+    showBadge: true,
+  );
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
   runApp(MyApp(settingsController: settingsController));
 }
